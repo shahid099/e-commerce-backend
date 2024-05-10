39d8b73cd91bcf365713bea6c03dd4b4661c456f
Muhammad Shahid <108601827+shahid099@users.noreply.github.com>
2024-05-10 23:35:22 +0500

    Added versions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import * as dotenv from 'dotenv';
 const app = express();
 app.use(express.json({ limit: '50mb' }));
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
